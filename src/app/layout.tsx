72b798339199f22937f651eb8ec518a29796e389
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-22 10:38:37 +0700

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
       <SessionProvider>
         <body className={inter.className}>{children}</body>
       </SessionProvider>
+      <Toaster />
     </html>
   );
 }
