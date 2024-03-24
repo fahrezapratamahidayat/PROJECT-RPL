bd29d079aa35ccf33c5c36df7598144a90d4fac1
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-24 21:27:21 +0700

    build: label input container

---
+++ src/components/layouts/labelInputContainer.tsx
@@ -0,0 +1,9 @@
+export default function LabelInputContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="space-y-2">
+        {children}
+      </div>
+    </>
+  );
+}
