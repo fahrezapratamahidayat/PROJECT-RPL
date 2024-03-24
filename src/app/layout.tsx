2f5814e2ad2e7b6949bf96cb9c5ab60dd7c7a064
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-03-24 21:23:24 +0700

    feat: top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-import { Toaster } from "@/components/ui/toaster";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,13 +12,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  ``
+  ``;
   return (
     <html lang="en">
-      <SessionProvider>
-        <body className={inter.className}>{children}</body>
-      </SessionProvider>
-      <Toaster />
+      <body className={inter.className}>
+        <NextTopLoader
+          color="#fff"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={2}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          zIndex={1600}
+          showAtBottom={false}
+        />
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
