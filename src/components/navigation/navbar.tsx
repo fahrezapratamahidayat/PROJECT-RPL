8e1aece22d09053561893b4d14aa409ce6712483
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-04-09 15:31:56 +0700

    "Add navigation links to Navbar component"
    This commit adds navigation links to the Navbar component in a project using Next.js and Lucide-react icons. The new links include Home, Projects, Chats, and Calendar. The active link is highlighted based on the current pathname. The code also includes the necessary imports for Link component from Next.js and usePathname hook from Next.navigation.

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -4,8 +4,17 @@ import { AlignRight, Bell } from "lucide-react";
 import { DropDownAddNavbar } from "../dropdown/dropDownAddNavbar";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
+import {
+  CalendarFold,
+  Home,
+  MessageSquareMore,
+  StickyNote,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <>
       <div className="sticky top-0 flex left-0 z-10 max-w-full w-full items-center justify-between  lg:flex lg:px-7 px-3 py-3 border-b backdrop-blur-lg">
@@ -17,8 +26,46 @@ export default function Navbar() {
             </Button>
           </SheetTrigger>
           <SheetContent side="left" className="sm:max-w-xs">
+            <h1 className="text-2xl font-semibold">Task</h1>
             <nav className="grid gap-6 text-lg font-medium">
-              <h1>test</h1>
+              <div className="leading-6 space-y-3 py-10">
+                <Link
+                  href="/home"
+                  className={`${
+                    pathname === "/home" ? "bg-secondary" : ""
+                  } flex items-center px-4 py-3 gap-5 hover:bg-secondary rounded-md font-semibold cursor-pointer`}
+                >
+                  <Home className="h-5 w-5" />
+                  <span className="text-sm">Home</span>
+                </Link>
+                <Link
+                  href="/projects"
+                  className={`${
+                    pathname === "/projects" ? "bg-secondary" : ""
+                  } flex items-center px-4 py-3 gap-5 hover:bg-secondary rounded-md font-semibold cursor-pointer`}
+                >
+                  <StickyNote className="h-5 w-5" />
+                  <span className="text-sm">Projects</span>
+                </Link>
+                <Link
+                  href="/chats"
+                  className={`${
+                    pathname.startsWith("/chats") ? "bg-secondary" : ""
+                  } flex items-center px-4 py-3 gap-5 hover:bg-secondary rounded-md font-semibold cursor-pointer`}
+                >
+                  <MessageSquareMore className="h-5 w-5" />
+                  <span className="text-sm">Chats</span>
+                </Link>
+                <Link
+                  href="/"
+                  className={`${
+                    pathname === "/timeline" ? "bg-secondary" : ""
+                  } flex items-center px-4 py-3 gap-5 hover:bg-secondary rounded-md font-semibold cursor-pointer`}
+                >
+                  <CalendarFold className="h-5 w-5" />
+                  <span className="text-sm">Calendar</span>
+                </Link>
+              </div>
             </nav>
           </SheetContent>
         </Sheet>
