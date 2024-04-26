52e8d4ff8dbb7980300aa45339cba883330e5650
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-04-26 20:28:03 +0700

    add: routing schedule and slug

---
+++ src/app/schedule/[[...slug]]/page.tsx
@@ -0,0 +1,16 @@
+export default function Page({ params }: { params: { slug: string } }) {
+  const task = {
+    title: "ss",
+    description: "sss",
+    priority: "tinggi",
+    statusTask: "In Progress",
+    dueDate: "2024-04-24T20:37",
+    category: ["administratif", "pengembangan_produk"],
+    typeTask: "personal",
+  };
+  return (
+    <>
+
+    </>
+  );
+}
