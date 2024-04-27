2009da8fee2a9a0dd4fb0879b6c6ea7abd45d87c
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-04-27 18:05:21 +0700

    Remove Unused Import

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,13 +1,10 @@
-import ChatUi from "@/components/chat/chatUi";
+
 import ConverSationList from "@/components/chat/conversationList";
-import { DarkModeToggle } from "@/components/dark-mode-toggle";
 import ChatPanelLayout from "@/components/layouts/chatPanelLayout";
-import PagesLayouts from "@/components/layouts/pagesLayouts";
 import Navbar from "@/components/navigation/navbar";
 import Sidebar from "@/components/navigation/sidebar";
 import React from "react";
 
-const text: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 export default function PageChats() {
   return (
     <>
