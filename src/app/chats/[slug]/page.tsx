a2070e43c04295880ace0c07b5dc5928365e0d32
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-04-27 18:06:39 +0700

    refactor

--- src/app/chats/[slug]/page.tsx
+++ src/app/chats/[slug]/page.tsx
@@ -1,15 +1,7 @@
-"use client";
 import ChatRoomList from "@/components/chat/ChatRoomList";
-import ChatUi from "@/components/chat/chatUi";
 import ConverSationList from "@/components/chat/conversationList";
-import { DarkModeToggle } from "@/components/dark-mode-toggle";
-import ChatPanelLayout from "@/components/layouts/chatPanelLayout";
-import PagesLayouts from "@/components/layouts/pagesLayouts";
 import Navbar from "@/components/navigation/navbar";
 import Sidebar from "@/components/navigation/sidebar";
-import { firestore } from "@/lib/firebase/init";
-import { collection, doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
 
 type ChatSlug = {
   params: {
@@ -17,40 +9,8 @@ type ChatSlug = {
   };
 };
 
-type User = {
-  id: string;
-  fullname: string;
-  email: string;
-  idp: string;
-  profileUrl: string;
-  created_At: Date;
-};
-
-export default function PageChats(props: ChatSlug) {
+export default async function PageChats(props: ChatSlug) {
   const { params } = props;
-  const [userChats, setUserChats] = useState([] as User[]);
-  useEffect(() => {
-    const fetchUserById = async () => {
-      const userId = params.slug;
-      try {
-        const userDocRef = doc(collection(firestore, "users"), params.slug);
-        const userDocSnapshot = await getDoc(userDocRef);
-
-        if (userDocSnapshot.exists()) {
-          const userData: any = {
-            id: userDocSnapshot.id,
-            ...userDocSnapshot.data(),
-          };
-          setUserChats(userData);
-        } else {
-        }
-      } catch (error) {
-        console.error("Error fetching user document:", error);
-      }
-    };
-
-    fetchUserById();
-  }, [params.slug]);
   return (
     <>
       <div className="flex min-h-screen w-full flex-col ">
@@ -60,7 +20,7 @@ export default function PageChats(props: ChatSlug) {
           <div className="flex gap-2 lg:py-7 lg:px-7 items-start h-full w-ful">
             <ConverSationList slug={params.slug} className="lg:flex hidden" />
             {!params.slug ? null : (
-              <ChatRoomList data={userChats} slug={params.slug} />
+              <ChatRoomList slug={params.slug} />
             )}
           </div>
         </div>
