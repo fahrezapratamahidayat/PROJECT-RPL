aa65423d479d2b9f429be041c66f01d669b5a714
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-08 22:36:37 +0700

    add: tabs value to localStorange

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-export default function useLocalStorage(key: string, initialValue: any) {
-  const [storedValue, setStoredValue] = useState(initialValue);
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
 
-  const setValue = (value: any) => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
+  useEffect(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      setStoredValue(item ? JSON.parse(item) as T : initialValue);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [key, initialValue]);
+
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return [storedValue, setValue];
 }
 
+export default useLocalStorage;
