54cd522d1f8c668571df630fcd0149e619654e3d
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-12 23:32:04 +0700

    add: redirect, andd detailtaskPage

--- src/app/schedule/[[...slug]]/page.tsx
+++ src/app/schedule/[[...slug]]/page.tsx
@@ -1,16 +1,23 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  const task = {
-    title: "ss",
-    description: "sss",
-    priority: "tinggi",
-    statusTask: "In Progress",
-    dueDate: "2024-04-24T20:37",
-    category: ["administratif", "pengembangan_produk"],
-    typeTask: "personal",
-  };
+import Navbar from "@/components/navigation/navbar";
+import Sidebar from "@/components/navigation/sidebar";
+import { redirect } from 'next/navigation'
+import DetailsTaskPage from "@/components/pages/detailsTaskPage";
+
+export default function Page({ params }: { params: { slug: string[] } }) {
+
+  if (!params.slug || params.slug.length === 0) {
+    redirect('/dashboard')
+    return;
+  }
   return (
     <>
-
+     <div className="flex min-h-screen w-full flex-col ">
+      <Sidebar />
+      <div className="flex flex-col lg:pl-[14rem]">
+        <Navbar />
+        <DetailsTaskPage params={params} />
+      </div>
+    </div>
     </>
   );
 }
