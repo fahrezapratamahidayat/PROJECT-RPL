98a1016b1a24a8059b073952b0e31d4b0dcb260d
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-12 23:35:22 +0700

    add: card task dashboard

--- src/components/pages/dashboardView.tsx
+++ src/components/pages/dashboardView.tsx
@@ -5,74 +5,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { greetings } from "@/hooks/greetings";
 import CardTeams from "../card/cardTeams";
 import CardTasksTeams from "../card/cardTasksTeams";
+import CardTaskDashboard from "../card/cardTaskDashboard";
 export default function DashboardView() {
   return (
     <>
       <main className="py-7 px-3 lg:px-7 flex flex-col gap-4">
-        <div className="border flex flex-col justify-center gap-3 px-3 py-2 rounded-lg w-full">
-          <div className="flex flex-col space-y-1">
-            <h1 className="text-lg font-bold ">Dashboard</h1>
-            <p className="text-sm text-muted-foreground">
-             Halo, {greetings()} 
-            </p>
-          </div>
-          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-            <Card x-chunk="dashboard-01-chunk-0">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Total Tasks
-                </CardTitle>
-                <DollarSign className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">0</div>
-                <p className="text-xs text-muted-foreground">
-                  0.1% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card x-chunk="dashboard-01-chunk-1">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  In Proggres
-                </CardTitle>
-                <Users className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+2350</div>
-                <p className="text-xs text-muted-foreground">
-                  +180.1% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card x-chunk="dashboard-01-chunk-2">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Pending</CardTitle>
-                <CreditCard className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+12,234</div>
-                <p className="text-xs text-muted-foreground">
-                  +19% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card x-chunk="dashboard-01-chunk-3">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Completed
-                </CardTitle>
-                <Activity className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+573</div>
-                <p className="text-xs text-muted-foreground">
-                  +201 since last hour
-                </p>
-              </CardContent>
-            </Card>
-          </div>
-        </div>
+        <CardTaskDashboard />
         <div className="w-full flex lg:flex-row sm:flex-row md:flex-row flex-col gap-2">
           <CardListTasks />
           <CardTasksTeams />
