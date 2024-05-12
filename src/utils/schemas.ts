882527548109d86f45a43453b829d007b9fad2a6
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-12 23:31:27 +0700

    add: schema subTask

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -18,11 +18,31 @@ export const schemaAddTasksExtended = z.object({
     attachments: z.string().min(1, { message: "Attachments is required" }),
 });
 
+export const schemaEditTasks = z.object({
+  title: z.string().min(1, { message: "Title is required" }).optional(),
+  description: z.string().min(1, { message: "Description is required" }).optional(),
+  typeTask: z.enum(["personal", "teams"]).refine(val => ["personal", "teams"].includes(val), { message: "Type Task is required" }).optional(),
+  dueDate: z.string().min(1, { message: "Due Date is required" }).optional(),
+  dueTime: z.string().min(1, { message: "Due Time is required" }).optional(),
+  priority: z.enum(["tinggi", "sedang", "kecil"]).refine(val => ["tinggi", "sedang", "kecil"].includes(val), { message: "Priority is required" }).optional(),
+  assigned: z
+    .array(z.string().min(1))
+    .max(10)
+    .optional(),
+    statusTask: z.enum(["on going", "completed", "pending", "cancel"]).refine(val => ["on going", "completed", "pending", "cancel"].includes(val), { message: "Status Task is required" }).optional(),
+});
 
 export const schemaAddTeamsExtended = z.object({
   name: z.string().min(1, { message: "Name is required" }),
   description: z.string().min(1, { message: "Description is required" }),
   members: z.array(z.string()).min(1, { message: "Members is required" }),
+})
+
+export const schemaAddSubstask = z.object({
+  subname: z.string().min(1, { message: "Name is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  dueDate: z.string().min(1, { message: "Due Date is required" }),
+  dueTime: z.string().min(1, { message: "Due Time is required" }),
 })
 
 // members: z.array(z.object({
