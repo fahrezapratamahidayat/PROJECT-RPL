20be889abd02943adbbe8b8e5ba1107eac847de2
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-13 22:27:50 +0700

    update with env

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -28,12 +28,12 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKGlrVJEh8m_3Haz-WGKoy318BpnuQNJs",
-  authDomain: "next-app-rpl.firebaseapp.com",
-  projectId: "next-app-rpl",
-  storageBucket: "next-app-rpl.appspot.com",
-  messagingSenderId: "21858421502",
-  appId: "1:21858421502:web:93e28c3923ae5e2ce733af"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app;
