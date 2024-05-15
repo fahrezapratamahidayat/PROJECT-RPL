ed9f275ffef60f0a4d10a8fbc490fb3984f95247
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-15 22:42:17 +0700

    add: avatar group and variant

--- src/components/avatars/avatarGroup.tsx
+++ src/components/avatars/avatarGroup.tsx
@@ -1,35 +1,111 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import Link from "next/link";
+import { Users } from "lucide-react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
-export default function AvatarGroup({
-  data,
-  slug,
-}: {
+const buttonVariants = cva(
+  "flex items-centers justify-start gap-5 px-3 py-3 rounded-lg",
+  {
+    variants: {
+      variant: {
+        default: "bg-muted",
+        destructive: "bg-destructive",
+        outline: "bg-transparent",
+        secondary: " border",
+        ghost: "bg-transparent",
+        link: "bg-transparent",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+export interface AvatarGroupProps extends VariantProps<typeof buttonVariants> {
   data: {
     id: string;
     name: string;
     profileUrl?: string;
+    membersInGroup?: string[] | number;
+    leader: string;
+    description: string;
+    members: string[];
+    created_At?: Date;
   };
-  slug?: string;
-}) {
-  return (
-    <>
-      <Link href={`/chats/${data.id}?type=group`}>
-        <div
-          className={`${
-            data.id === slug ? "bg-muted" : ""
-          } flex items-centers justify-start gap-5 px-3 py-3 rounded-lg hover:bg-muted`}
-        >
-          <Avatar>
-            <AvatarImage src={data.profileUrl || "https://github.com/shadcn.png"} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+  slug: string;
+  hrefActive?: boolean;
+  className?: string;
+}
+
+const AvatarGroups = React.forwardRef<HTMLAnchorElement, AvatarGroupProps>(
+  (
+    {
+      data,
+      slug,
+      hrefActive = false,
+      variant,
+      className,
+      ...props
+    }: AvatarGroupProps,
+    ref
+  ) => {
+    return (
+      <Link
+        href={hrefActive ? `/teams/${data.id}` : ""}
+        className={cn(buttonVariants({ variant, className }))}
+        {...props}
+        ref={ref}
+      >
+        <Avatar>
+          <AvatarImage
+            src={data.profileUrl || "https://github.com/shadcn.png"}
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="">
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold">{data.name}</h1>
+          </div>
+          {data.membersInGroup && (
             <div className="flex items-center">
-              <h1 className="text-lg font-semibold">{data.name}</h1>
+              <Users className="mr-2 h-4 w-4" />
+              <p className="text-sm text-muted-foreground">
+                {data.membersInGroup} members
+              </p>
             </div>
+          )}
         </div>
       </Link>
-    </>
-  );
-}
+    );
+  }
+);
+AvatarGroups.displayName = "AvatarGroups";
+export { AvatarGroups };
+// export default function AvatarGroup({
+//   data,
+//   slug,
+//   hrefActive,
+// }: {
+//   data: {
+//     id: string;
+//     name: string;
+//     profileUrl?: string;
+//     membersInGroup?: string[] | number;
+//     leader: string;
+//     description: string;
+//     members: string[];
+//     created_At?: Date;
+//     updated_At?: Date;
+//   };
+//   slug?: string;
+//   hrefActive?: boolean;
+
+// }) {
+//   return (
+//     <>
+
+//     </>
+//   );
+// }
