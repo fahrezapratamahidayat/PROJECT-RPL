6c7fc1bd086984308acbb7827eb464d62da4a480
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-19 22:35:26 +0700

    remove: unused function

--- src/app/schedule/[[...slug]]/page.tsx
+++ src/app/schedule/[[...slug]]/page.tsx
@@ -1,23 +1,23 @@
 import Navbar from "@/components/navigation/navbar";
 import Sidebar from "@/components/navigation/sidebar";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import DetailsTaskPage from "@/components/pages/detailsTaskPage";
+import { CircularProgressProps } from "@mui/material/CircularProgress";
 
 export default function Page({ params }: { params: { slug: string[] } }) {
-
   if (!params.slug || params.slug.length === 0) {
-    redirect('/dashboard')
+    redirect("/dashboard");
     return;
   }
   return (
     <>
-     <div className="flex min-h-screen w-full flex-col ">
-      <Sidebar />
-      <div className="flex flex-col lg:pl-[14rem]">
-        <Navbar />
-        <DetailsTaskPage params={params} />
+      <div className="flex min-h-screen w-full flex-col ">
+        <Sidebar />
+        <div className="flex flex-col lg:pl-[14rem]">
+          <Navbar />
+          <DetailsTaskPage params={params} />
+        </div>
       </div>
-    </div>
     </>
   );
 }
