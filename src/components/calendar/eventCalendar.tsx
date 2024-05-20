70267e810c3013f1ae51346ea2853df2ecf6736b
fahrezapratamh <fahrezaxgamer@gmail.com>
2024-05-20 11:14:48 +0700

    add: scroll area for mobile

--- src/components/calendar/eventCalendar.tsx
+++ src/components/calendar/eventCalendar.tsx
@@ -15,6 +15,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import CalendarPopover from "./calendar";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 
 // const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const WEEKDAYS = [
@@ -125,7 +126,6 @@ export function CalendarEvent({ events }: EventCalendarProps) {
     setCurrentDate(setMonth(currentDate, newMonth));
   };
 
-  
   const decrementMonth = () => {
     const newMonth = month - 1;
     handleMonthChange(newMonth >= 0 ? newMonth : 11);
@@ -146,7 +146,7 @@ export function CalendarEvent({ events }: EventCalendarProps) {
 
   return (
     <>
-      <div className="p-4">
+      <div className="p-4 w-full">
         <div className="mb-4 flex justify-between items-center">
           <CalendarPopover
             open={popoverOpen}
@@ -159,96 +159,115 @@ export function CalendarEvent({ events }: EventCalendarProps) {
             onMonthChange={handleMonthChangePopover}
           />
           <div className="flex items-center gap-2">
-            <Button variant={"ghost"} size={"icon"} className="p-2 rounded-full transition-colors" onClick={decrementMonth}>
+            <Button
+              variant={"ghost"}
+              size={"icon"}
+              className="p-2 rounded-full transition-colors"
+              onClick={decrementMonth}
+            >
               <ChevronLeftIcon className="w-4 h-4" />
             </Button>
-            <Button variant={"ghost"} size={"icon"} className="p-2 rounded-full transition-colors" onClick={incrementMonth}>
+            <Button
+              variant={"ghost"}
+              size={"icon"}
+              className="p-2 rounded-full transition-colors"
+              onClick={incrementMonth}
+            >
               <ChevronRightIcon className="w-4 h-4" />
             </Button>
           </div>
         </div>
-        <div className="grid grid-cols-7">
-          {WEEKDAYS.map((day) => (
-            <div key={day} className="font-bold text-center mb-2">
-              {day}
-            </div>
-          ))}
-          {Array.from({ length: startingDayIndex }).map((_, index) => (
-            <div key={`empty-${index}`} className="border p-2 text-center" />
-          ))}
-          {daysInMonth.map((day, index) => {
-            const dateKey = format(day, "yyyy-MM-dd");
-            const todaysEvents = eventsByDate[dateKey] || [];
-            return (
-              <div
-                key={index}
-                className={cn(`border h-[300px] relative`, {
-                  "bg-secondarysss": todaysEvents.length > 0,
-                  border: !todaysEvents.some((event) => {
-                    const eventStart = format(event.dueDate, "yyyy-MM-dd");
-                    const eventEnd = event.dueTime
-                      ? format(event.dueTime, "yyyy-MM-dd")
-                      : eventStart;
-                    return dateKey >= eventStart && dateKey <= eventEnd;
-                  }),
-                })}
-              >
-                <div className="flex items-center justify-center px-3 py-2">
-                  <span
-                    className={cn("text-base text-white px-1.5 py-0.5 rounded-full", {
-                      " bg-blue-500": isToday(day),
-                    })}
-                  >
-                    {format(day, "d")}
-                  </span>
-                </div>
-                <div className="flex flex-col gap-1 absolute top-12 left-0 right-0">
-                  {todaysEvents.map((event) => {
-                    const eventStart = format(event.dueDate, "yyyy-MM-dd");
-                    const eventEnd = event.dueTime
-                      ? format(event.dueTime, "yyyy-MM-dd")
-                      : eventStart;
-                    if (dateKey === eventStart) {
-                      return (
-                        <div
-                          key={event.title}
-                          style={{ backgroundColor: event.attachments }}
-                          onClick={() => handleEventClick(event)}
-                          className="cursor-pointer p-2"
-                        >
-                          <span className="text-sm text-white font-semibold">
-                            {event.title}
-                          </span>
-                          {isDialogOpen &&
-                            selectedEvent &&
-                            selectedEvent.id === event.id && (
-                              <CardDialogEvents
-                                data={selectedEvent}
-                                onClose={closeDialog}
-                              />
-                            )}
-                        </div>
-                      );
-                    } else if (dateKey >= eventStart && dateKey <= eventEnd) {
-                      return (
-                        <div
-                          key={event.title}
-                          style={{ backgroundColor: event.attachments }}
-                          onClick={() => handleEventClick(event)}
-                          className="cursor-pointer"
-                        >
-                          <span className="opacity-0">{event.title}</span>
-                        </div>
-                      );
-                    } else {
-                      return null;
-                    }
-                  })}
-                </div>
+        <ScrollArea className="w-[600px] md:w-full sm:w-full lg:w-full">
+          <div className="grid grid-cols-7 md:w-full sm:w-full lg:w-full w-max">
+            {WEEKDAYS.map((day) => (
+              <div key={day} className="font-bold text-center mb-2">
+                {day}
               </div>
-            );
-          })}
-        </div>
+            ))}
+            {Array.from({ length: startingDayIndex }).map((_, index) => (
+              <div key={`empty-${index}`} className="border p-2 text-center" />
+            ))}
+            {daysInMonth.map((day, index) => {
+              const dateKey = format(day, "yyyy-MM-dd");
+              const todaysEvents = eventsByDate[dateKey] || [];
+              return (
+                <div
+                  key={index}
+                  className={cn(
+                    `border h-[300px] w-[150px] sm:w-full md:w-full lg:w-full relative`,
+                    {
+                      "bg-secondarysss": todaysEvents.length > 0,
+                      border: !todaysEvents.some((event) => {
+                        const eventStart = format(event.dueDate, "yyyy-MM-dd");
+                        const eventEnd = event.dueTime
+                          ? format(event.dueTime, "yyyy-MM-dd")
+                          : eventStart;
+                        return dateKey >= eventStart && dateKey <= eventEnd;
+                      }),
+                    }
+                  )}
+                >
+                  <div className="flex items-center justify-center px-3 py-2">
+                    <span
+                      className={cn(
+                        "text-base text-white px-1.5 py-0.5 rounded-full",
+                        {
+                          " bg-blue-500": isToday(day),
+                        }
+                      )}
+                    >
+                      {format(day, "d")}
+                    </span>
+                  </div>
+                  <div className="flex flex-col gap-1 absolute top-12 left-0 right-0">
+                    {todaysEvents.map((event) => {
+                      const eventStart = format(event.dueDate, "yyyy-MM-dd");
+                      const eventEnd = event.dueTime
+                        ? format(event.dueTime, "yyyy-MM-dd")
+                        : eventStart;
+                      if (dateKey === eventStart) {
+                        return (
+                          <div
+                            key={event.title}
+                            style={{ backgroundColor: event.attachments }}
+                            onClick={() => handleEventClick(event)}
+                            className="cursor-pointer p-2"
+                          >
+                            <span className="text-sm text-white font-semibold">
+                              {event.title}
+                            </span>
+                            {isDialogOpen &&
+                              selectedEvent &&
+                              selectedEvent.id === event.id && (
+                                <CardDialogEvents
+                                  data={selectedEvent}
+                                  onClose={closeDialog}
+                                />
+                              )}
+                          </div>
+                        );
+                      } else if (dateKey >= eventStart && dateKey <= eventEnd) {
+                        return (
+                          <div
+                            key={event.title}
+                            style={{ backgroundColor: event.attachments }}
+                            onClick={() => handleEventClick(event)}
+                            className="cursor-pointer"
+                          >
+                            <span className="opacity-0">{event.title}</span>
+                          </div>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
     </>
   );
